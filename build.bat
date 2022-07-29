a6d60e9f067ec9f375b4abfc52682d9ffe30ac8d
f99mlu <mats.lundquist75@gmail.com>
2022-07-29 10:49:32 +0200

    Added files from Desktop

---
+++ build.bat
@@ -0,0 +1,6 @@
+cd "C:\MSFS SDK\Tools\bin"
+rmdir /s /q c:\Temp\Packages\f99mlu-sweden-helipads
+fspackagetool.exe "c:\git-repo\swedish-helipads\f99mlu-sweden-helipadsproject.xml" -outputdir c:\temp
+rmdir /s /q %LOCALAPPDATA%\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-sweden-helipads
+xcopy /E /I c:\Temp\Packages\f99mlu-sweden-helipads %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-sweden-helipads
+rem xcopy /E /I c:\git-repo\swedish-helipads\PackageSources\modelLib %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-sweden-helipads\scenery\modelLib
