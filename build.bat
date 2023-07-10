e908d9d04fd3885486921585ea6243b877e0fe22
f99mlu <mats.lundquist75@gmail.com>
2023-07-10 11:27:05 +0200

    Update build paths

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 cd "C:\MSFS SDK\Tools\bin"
 rmdir /s /q c:\Temp\Packages\f99mlu-sweden-helipads
-fspackagetool.exe "c:\Server\git-repo\swedish-helipads\f99mlu-sweden-helipadsproject.xml" -outputdir c:\temp
-rmdir /s /q e:\MSFS\Community\f99mlu-sweden-helipads
-xcopy /E /I c:\Temp\Packages\f99mlu-sweden-helipads e:\MSFS\Community\f99mlu-sweden-helipads
+fspackagetool.exe "D:\git-repos\swedish-helipads\f99mlu-sweden-helipadsproject.xml" -outputdir c:\temp
+rmdir /s /q D:\MSFS\Community\f99mlu-sweden-helipads
+xcopy /E /I c:\Temp\Packages\f99mlu-sweden-helipads D:\MSFS\Community\f99mlu-sweden-helipads
 
 rem rmdir /s /q %LOCALAPPDATA%\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-sweden-helipads
 rem xcopy /E /I c:\Temp\Packages\f99mlu-sweden-helipads %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-sweden-helipads
