fe9a6592eb3aa4ac2e52f19fc1a61f42a215ba50
f99mlu <mats.lundquist75@gmail.com>
2024-11-20 23:20:56 +0100

    Add MSFS2024 build script

--- build.bat
+++ build.bat
@@ -3,9 +3,3 @@ rmdir /s /q c:\Temp\Packages\f99mlu-sweden-helipads
 fspackagetool.exe "D:\git-repos\swedish-helipads\f99mlu-sweden-helipadsproject.xml" -outputdir c:\temp
 rmdir /s /q D:\MSFS\Community\f99mlu-sweden-helipads
 xcopy /E /I c:\Temp\Packages\f99mlu-sweden-helipads D:\MSFS\Community\f99mlu-sweden-helipads
-
-rem rmdir /s /q %LOCALAPPDATA%\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-sweden-helipads
-rem xcopy /E /I c:\Temp\Packages\f99mlu-sweden-helipads %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-sweden-helipads
-
-
-rem xcopy /E /I c:\git-repo\swedish-helipads\PackageSources\modelLib %HOMEPATH%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\f99mlu-sweden-helipads\scenery\modelLib
---
+++ build2024.bat
@@ -0,0 +1,5 @@
+cd "C:\MSFS 2024 SDK\Tools\bin"
+rmdir /s /q c:\Temp\Packages\f99mlu-sweden-helipads
+fspackagetool.exe "D:\git-repos\swedish-helipads\f99mlu-sweden-helipadsproject.xml" -outputdir c:\temp
+rmdir /s /q E:\MSFS2024\Community\f99mlu-sweden-helipads
+xcopy /E /I c:\Temp\Packages\f99mlu-sweden-helipads E:\MSFS2024\Community\f99mlu-sweden-helipads
